d054bcd5ef6a53554d30f6b2f67948b6d809d046
tjmann95 <tjmann95@gmail.com>
2017-05-31 16:51:19 -0400

    Update shader_loader.py

--- shader_loader.py
+++ shader_loader.py
@@ -1,51 +1,43 @@
 from OpenGL.GL import *
-import OpenGL.GL.shaders
-
 
 class Shader(object):
 
     def __init__(self, vertex, fragment):
-        self.vertex_shader_file = vertex
-        self.fragment_shader_file = fragment
+        self.vertex_path = vertex
+        self.fragment_path = fragment
 
-        self.compile_shader()
+        self.shader_program = self.compile_shader()
 
-    def load_shader(self, file):
-        # shader_source = ""
-        with open(file) as f:
-            shader_source = f.read()
+    def load_source(self, file):
+        f = open(file, "r")
+        source = f.read()
         f.close()
-        return str.encode(shader_source)
+
+        return source
 
     def compile_shader(self):
-        vert_shader = self.load_shader(self.vertex_shader_file)
-        frag_shader = self.load_shader(self.fragment_shader_file)
+        vertex_shader_source = self.load_source(self.vertex_path)
+        fragment_shader_source = self.load_source(self.fragment_path)
 
         # Compile vertex shader
         vertex_shader = glCreateShader(GL_VERTEX_SHADER)
-        glShaderSource(vertex_shader, 1, vert_shader, None)
+        glShaderSource(vertex_shader, vertex_shader_source)
         glCompileShader(vertex_shader)
 
         # Check vertex compilation success
-        vert_success = 0
-        vert_info_log = ""
-        glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, vert_success)
-        if not vert_success:
-            glGetShaderInfoLog(vertex_shader, 512, None, vert_info_log)
-            print("ERROR: Vertex shader failed to compile:\n" + vert_info_log)
+        error_message = glGetShaderInfoLog(vertex_shader)
+        if not "No errors." in str(error_message):
+            print("ERROR: Vertex shader failed to compile:\n" + str(error_message))
 
         # Compile fragment shader
         fragment_shader = glCreateShader(GL_FRAGMENT_SHADER)
-        glShaderSource(fragment_shader, 1, frag_shader, None)
+        glShaderSource(fragment_shader, fragment_shader_source)
         glCompileShader(fragment_shader)
 
         # Check fragment compilation success
-        frag_success = 0
-        frag_info_log = ""
-        glGetShaderiv(fragment_shader, 512, None, frag_info_log)
-        if not frag_success:
-            glGetShaderInfoLog(fragment_shader, 512, None, frag_info_log)
-            print("ERROR: Fragment shader failed to compile:\n" + frag_info_log)
+        error_message = glGetShaderInfoLog(fragment_shader)
+        if not "No errors." in str(error_message):
+            print("ERROR: Fragment shader failed to compile:\n" + str(error_message))
 
         # Creating shader program
         shader_program = glCreateProgram()
@@ -54,12 +46,9 @@ class Shader(object):
         glLinkProgram(shader_program)
 
         # Check shader program success
-        program_success = 0
-        program_info_log = ""
-        glGetProgramiv(shader_program, GL_LINK_STATUS, program_success)
-        if not program_success:
-            glGetProgramInfoLog(shader_program, 512, None, program_info_log)
-            print("ERROR: Shader program failed:\n" + program_info_log)
+        error_message = glGetProgramInfoLog(shader_program)
+        if not "No errors." in str(error_message):
+            print("ERROR: Shader program failed:\n" + str(error_message))
 
         glDeleteShader(vertex_shader)
         glDeleteShader(fragment_shader)
