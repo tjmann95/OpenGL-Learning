6fec92a90ad1b6d3b18d01826a0cf3f9fb704a5b
tjmann95 <tjmann95@gmail.com>
2017-06-01 16:48:34 -0400

    Update fragment.fs

--- fragment.fs
+++ fragment.fs
@@ -1,13 +1,11 @@
 #version 330
-in vec3 vertexColor;
 in vec2 texCoordOut;
 
 out vec4 color;
 
-uniform sampler2D ourTexture1;
-uniform sampler2D ourTexture2;
+uniform sampler2D ourTexture;
 
 void main()
 {
-    color = mix(texture(ourTexture1, texCoordOut), texture(ourTexture2, texCoordOut), .2);
+    color = texture(ourTexture, texCoordOut);
 }
