eab56a52745520f7ba97d1b868e330433e98bfe6
tjmann95 <tjmann95@gmail.com>
2017-06-01 23:56:23 -0400

    Update fragment.fs

--- fragment.fs
+++ fragment.fs
@@ -1,15 +1,33 @@
 #version 330
-layout (location = 0) in vec3 position;
-layout (location = 1) in vec2 texCoord;
+//in vec2 texCoordOut;
+in vec3 Normal;
+in vec3 FragPos;
 
-out vec2 texCoordOut;
+out vec4 outColor;
 
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
+//uniform sampler2D samplerTexture;
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
 
 void main()
 {
-    gl_Position = projection * view* model * vec4(position, 1.0);
-    texCoordOut = texCoord;
+    float ambientStrength = .1;
+    vec3 ambient = ambientStrength * lightColor;
+
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    float specularStrength = .5;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;
+
+    //outColor = texture(samplerTexture, texCoordOut);
+    vec3 result = (ambient + diffuse + specular) * objectColor;
+    outColor = vec4(result, 1.0);
 }
